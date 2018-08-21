ab8005250d25e8fad7dd94fb282fd07a9bd41de1
ErrolYan <2681506@gmail.com>
2018-08-21 15:27:34 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import re
 from aip import AipSpeech
-import time
 
 APP_ID = '11478815'
 API_KEY = '2m4bO8OV8F21saqe96H8Q0MV'
@@ -11,34 +10,30 @@ client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
 
 # txt_path = '重生六零空间纪事.txt'
 txt_path = '重生之爸爸不好当.txt'
-
-# with open(txt_path, 'r', encoding='utf8') as f:
-#     text = f.read()
-#     text = re.sub(r'(.{30})', lambda x: '{}\n'.format(x.group(1)), text)
-
-# with open(txt_path, 'w', encoding='utf8') as f:
-#     f.write(text)
-
-with open(txt_path, 'r', encoding='utf8') as f:
-    for index, line in enumerate(f):
-        index = '2B%06d'%index
-        # if index < 8331:
-        #     continue
+with open(txt_path, encoding='utf8') as f:
+    index = 1
+    for line in f:
         line = line.strip()
 
-        try:
-            res = client.synthesis(line, 'zh', 1, {'per': '4', 'spd': '5', 'vol': '7', 'aue': '6'})
-        except Exception:
-            time.sleep(5)
-            res = client.synthesis(line, 'zh', 1, {'per': '4', 'spd': '5', 'vol': '7', 'aue': '6'})
-        if not isinstance(res, dict):
-            with open('./wav/{}.wav'.format(index), 'wb') as f:
-                f.write(res)
-                
-            with open('./txt/{}.txt'.format(index), 'w') as f:
-                f.write(line)
-        else:
-            print(index, 'err')
+        if line != '':
+            match = re.match(r'第(\d+)章', line)
+            if match:
+                cha_num = match.group(1)
+                index = 1
+                continue
 
-        print(index)            
-        # index += 1
+            if int(cha_num) < 17:
+                continue
+
+            res = client.synthesis(line, 'zh', 1, {'per': '4', 'spd': '5', 'vol': '7', 'aue': '6'})
+            if not isinstance(res, dict):
+                with open('./wav/{}_{}.wav'.format(cha_num, index), 'wb') as f:
+                    f.write(res)
+                    
+                with open('./txt/{}_{}.txt'.format(cha_num, index), 'w') as f:
+                    f.write(line)
+            else:
+                print(cha_num, index, 'err')
+
+            print(cha_num, index)            
+            index += 1
